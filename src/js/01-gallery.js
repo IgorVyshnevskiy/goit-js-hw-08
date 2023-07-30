001112260d2e37ba9483a4f48763ff8e44021471
Igor Vyshnevskyi <igor.vyshnevskiy097@gmail.com>
2023-07-30 15:29:38 +0300

    finished first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector('.gallery');
+function galleryItemsEl() {
+  const list = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__link" href="${original}">
+          <img class="gallery__image" 
+            src="${preview}" 
+            data-source="${original}" 
+            alt="${description}" />
+        </a>`
+    )
+    .join('');
+
+  galleryContainerEl.innerHTML = list;
+}
+galleryItemsEl();
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
