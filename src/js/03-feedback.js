26fb3fe551a5d36097220345b3a7681406ee404f
Igor Vyshnevskyi <igor.vyshnevskiy097@gmail.com>
2023-07-31 22:37:57 +0300

    test2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,7 +5,7 @@ import throttle from 'lodash.throttle';
 const LOCAL_KEY = 'feedback-form-state';
 let formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
 
-form = document.querySelector('.feedback-form');
+form = document.getElementById('form');
 
 form.addEventListener('input', throttle(storageFormData, 500));
 form.addEventListener('submit', onFormSubmit);
